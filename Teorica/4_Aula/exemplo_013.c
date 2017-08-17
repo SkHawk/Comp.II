dbad3ac5571728fdf0faa14680cfe653ed58620e
SkHawk-PC <alexchamon@poli.ufrj.br>
2017-08-16 21:36:27 -0300

    Aula teorica 4

---
+++ Teorica/4_Aula/exemplo_013.c
@@ -0,0 +1,80 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e de Computacao
+ * EEL270 - Computacao II - Turma 2017/2
+ * Prof. Marcelo Luiz Drumond Lanza
+ *
+ * $Author$
+ * $Date$
+ * $Log$
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUMERO_ARGUMENTOS		2
+
+#define OK				0
+#define NUMERO_ARGUMENTOS_INVALIDO	1
+#define ARGUMENTO_INVALIDO		2
+#define NUMERO_EXCEDE_MAXIMO		3
+
+#define VALOR_MAXIMO_UNSIGNED_SHORT	65535
+
+#define EOS				'\0'
+
+unsigned long long
+CalcularFatorial (unsigned short);
+
+unsigned long long
+CalcularFatorial (unsigned short numero)
+{
+  unsigned long long fatorial;
+  
+  fatorial = 1;
+  while (numero > 1)
+  {
+    fatorial = fatorial * numero;
+    numero--;
+    /* fatorial *= numero--; */
+  }
+
+  return fatorial;
+}
+
+
+int 
+main (int argc, char *argv [])
+{
+  int numero;
+  unsigned indice;
+
+  if (argc != NUMERO_ARGUMENTOS)
+  {
+    printf ("USO: %s <inteiro nao negativo>\n", argv [0]);
+    exit (NUMERO_ARGUMENTOS_INVALIDO);
+  }
+
+  for (indice = 0; argv [1][indice] != EOS; indice++)
+    if ((argv [1][indice] < '0') || (argv [1][indice] > '9'))
+    {
+      printf ("Argumento invalido !!!\n");
+      printf ("Primeiro caractere invalido: %c\n", argv [1][indice]);
+      exit (ARGUMENTO_INVALIDO);
+    }
+
+  numero = atoi (argv [1]);
+
+  if (numero > VALOR_MAXIMO_UNSIGNED_SHORT)
+  {
+    printf ("Valor excede o valor maximo: %i\n", VALOR_MAXIMO_UNSIGNED_SHORT);                      
+    exit (NUMERO_EXCEDE_MAXIMO);
+  }
+
+  printf ("%s!: %llu\n", argv [1], CalcularFatorial ((unsigned short) numero));
+
+  return OK;
+}
+
+/* $RCSfile$ */
---
+++ Teorica/4_Aula/exemplo_014.h
@@ -0,0 +1,21 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e de Computacao
+ * EEL270 - Computacao II - Turma 2017/2
+ * Prof. Marcelo Luiz Drumond Lanza
+ *
+ * $Author$
+ * $Date$
+ * $Log$
+ */
+
+#ifndef _EXEMPLO_014_H_
+#define _EXEMPLO_014_H_ 	"exemplo_014.h"
+
+unsigned long long
+CalcularFatorial (unsigned short);
+
+#endif
+
+/* $RCSfile$ */
