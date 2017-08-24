c2367af740e64e8fb19a9f835c147908c6c223e2
SkHawk-PC <alexchamon@poli.ufrj.br>
2017-08-24 01:02:49 -0300

    Aula pratica 2 - Biblioteca recursiva

---
+++ Pratica/2_Aula/aula0201a.c
@@ -0,0 +1,39 @@
+/* 
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e de Computacao
+ * EEL270 - Computacao II - Turma 2017/2
+ * Prof. Marcelo Luiz Drumond Lanza
+ * Autor: Alexandre Alkmim Chamon
+ *
+ * $Author: chamon $
+ * $Date: 2017/08/24 02:36:44 $
+ * $Log: aula0201a.c,v $
+ * Revision 1.1  2017/08/24 02:36:44  chamon
+ * Initial revision
+ *
+ *
+ */
+
+#include "aula0201.h"
+
+ull
+CalcularMaximoDivisorComum (ull x, ull y)
+{
+ 
+  if ((x == 0) && (y == 0))
+    return 0;
+
+  if (x == 0)
+    return y;
+
+  if (y == 0)
+    return x;
+
+  if (x%y == 0)
+    return y;
+
+  return CalcularMaximoDivisorComum (y , x%y);
+}
+
+/* $RCSfile: aula0201a.c,v $ */
